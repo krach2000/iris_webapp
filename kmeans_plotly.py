cc1e22dbfe05862e15e7c14802b98a54b87762cd
Keerthi R Rachamallu <keerthi.r.rachamallu@pwc.com>
2020-11-25 19:11:43 +0000

    graph displaying

--- kmeans_plotly.py
+++ kmeans_plotly.py
@@ -16,14 +16,23 @@ def apply_kmeans_(X_var, Y_var, n_clusters):
     X = iris.data[:, [int(X_var), int(Y_var)]]
     k_means = KMeans(n_clusters=int(n_clusters), random_state=42)
     y_means = k_means.fit_predict(X)
-    data = []
+    traces = []
+    data = {}
+
     hover_label = [iris.feature_names[X_var],iris.feature_names[Y_var]]
     for i in range(int(n_clusters)):
-        data.append(pgo.Scatter(x=X[y_means == i, 0], y=X[y_means == i, 1], mode="markers", marker=pgo.Marker(size=12),
+        traces.append(pgo.Scatter(x=X[y_means == i, 0], y=X[y_means == i, 1], mode="markers", marker=pgo.Marker(size=12),
                                 showlegend=False))
-    data.append(pgo.Scatter(x=k_means.cluster_centers_[:, 0], y=k_means.cluster_centers_[:, 1], mode="markers",
-                            marker=pgo.Marker(size=13, symbol='x'), showlegend=False), )
+        x_name = "XT" + str(i)
+        y_name = "YT" + str(i)
+        data[x_name] = X[y_means == i, 0].tolist()
+        data[y_name] = X[y_means == i, 1].tolist()
+    traces.append(pgo.Scatter(x=k_means.cluster_centers_[:, 0], y=k_means.cluster_centers_[:, 1], mode="markers",
+                            marker=pgo.Marker(size=13, symbol='x'), showlegend=False))
+    x_name = "XCC"
+    y_name = "YCC"
+    data[x_name] = k_means.cluster_centers_[:, 0].tolist()
+    data[y_name] = k_means.cluster_centers_[:, 1].tolist()
+    graphJSON = json.dumps(traces, cls=plotly.utils.PlotlyJSONEncoder)
 
-    graphJSON = json.dumps(data, cls=plotly.utils.PlotlyJSONEncoder)
-
-    return graphJSON
+    return data
--- main.py
+++ main.py
@@ -1,9 +1,4 @@
 from flask import Flask, render_template, session, redirect, url_for, session, send_file, request, jsonify
-from flask_wtf import FlaskForm
-from wtforms import (StringField, BooleanField, DateTimeField,
-                     RadioField, SelectField, TextField,
-                     TextAreaField, SubmitField)
-from wtforms.validators import DataRequired
 from kmeans import *
 from kmeans_plotly import *
 import numpy as np
@@ -16,40 +11,31 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'mysecretkey'
 
 
-class inputForm(FlaskForm):
-    x_var = SelectField('Please choose X-variable: ',
-                        choices=[('0', 'Sepal Width'), ('1', 'Sepal Length'), ('2', 'Petal Width'),
-                                 ('3', 'Petal Length')])
-    y_var = SelectField('Please choose Y-variable: ',
-                        choices=[('0', 'Sepal Width'), ('1', 'Sepal Length'), ('2', 'Petal Width'),
-                                 ('3', 'Petal Length')])
-    cluster_num = SelectField('Please choose number of clusters:',
-                              choices=[('1', '1'), ('2', '2'), ('3', '3'), ('4', '4'), ('5', '5')])
-    submit = SubmitField('Submit')
-
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    # form = inputForm()
-    graphJSON = None
-    graph = None
 
-    if request.method == "POST":
-        x_var = request.form['x_var']
-        y_var = request.form['y_var']
-        cluster_num = request.form.get('cluster_num')
+    #if request.method == "POST":
+        #x_var = request.form['x_var']
+        #y_var = request.form['y_var']
+        #cluster_num = request.form.get('cluster_num')
         # graph = apply_kmeans(int(session['x_var']), int(session['y_var']), int(session['cluster_num']))
 
-        graphJSON = apply_kmeans_(int(x_var), int(y_var), int(cluster_num))
+        #graphJSON = apply_kmeans_(int(x_var), int(y_var), int(cluster_num))
 
-        return render_template('index.html', graph=graph, graphJSON=graphJSON)
+        #return render_template('index.html', graph=None, graphJSON=graphJSON)
 
     return render_template('index.html', graph=None, graphJSON=None)
 
+
+@app.route('/cluster_num', methods=['GET', 'POST'])
 def cluster_num():
-    return jsonify({"cluster_num": request.json["cluster_num"]})
-
-
+    print(request.json["cluster_num"])
+    x_var = 1
+    y_var = 2
+    cluster_num = int(request.json["cluster_num"])
+    data = apply_kmeans_(x_var, y_var, cluster_num)
+    return jsonify(data)
 
 if __name__ == '__main__':
     app.run(debug=True)
