b90d7c710de9b4511bee2403e5cb1240f75bac7f
krach2000 <keerthi.r.rachamallu@pwc.com>
2020-11-26 11:35:33 +0000

    final working version

--- kmeans_plotly.py
+++ kmeans_plotly.py
@@ -1,10 +1,5 @@
 from sklearn import datasets
 from sklearn.cluster import KMeans
-import json
-import plotly
-import chart_studio.plotly as py
-import plotly.graph_objs as pgo
-
 
 # sepal length - 0
 # sepal width - 1
@@ -16,23 +11,15 @@ def apply_kmeans_(X_var, Y_var, n_clusters):
     X = iris.data[:, [int(X_var), int(Y_var)]]
     k_means = KMeans(n_clusters=int(n_clusters), random_state=42)
     y_means = k_means.fit_predict(X)
-    traces = []
     data = {}
 
-    hover_label = [iris.feature_names[X_var],iris.feature_names[Y_var]]
     for i in range(int(n_clusters)):
-        traces.append(pgo.Scatter(x=X[y_means == i, 0], y=X[y_means == i, 1], mode="markers", marker=pgo.Marker(size=12),
-                                showlegend=False))
         x_name = "XT" + str(i)
         y_name = "YT" + str(i)
         data[x_name] = X[y_means == i, 0].tolist()
         data[y_name] = X[y_means == i, 1].tolist()
-    traces.append(pgo.Scatter(x=k_means.cluster_centers_[:, 0], y=k_means.cluster_centers_[:, 1], mode="markers",
-                            marker=pgo.Marker(size=13, symbol='x'), showlegend=False))
     x_name = "XCC"
     y_name = "YCC"
     data[x_name] = k_means.cluster_centers_[:, 0].tolist()
     data[y_name] = k_means.cluster_centers_[:, 1].tolist()
-    graphJSON = json.dumps(traces, cls=plotly.utils.PlotlyJSONEncoder)
-
     return data
--- main.py
+++ main.py
@@ -1,31 +1,14 @@
-from flask import Flask, render_template, session, redirect, url_for, session, send_file, request, jsonify
-from kmeans import *
+from flask import Flask, render_template, request, jsonify
 from kmeans_plotly import *
-import numpy as np
-import plotly
-import chart_studio.plotly as py
-import plotly.graph_objs as go
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'mysecretkey'
 
 
-
 @app.route('/', methods=['GET', 'POST'])
 def index():
-
-    #if request.method == "POST":
-        #x_var = request.form['x_var']
-        #y_var = request.form['y_var']
-        #cluster_num = request.form.get('cluster_num')
-        # graph = apply_kmeans(int(session['x_var']), int(session['y_var']), int(session['cluster_num']))
-
-        #graphJSON = apply_kmeans_(int(x_var), int(y_var), int(cluster_num))
-
-        #return render_template('index.html', graph=None, graphJSON=graphJSON)
-
-    return render_template('index.html', graph=None, graphJSON=None)
+    return render_template('index.html')
 
 
 @app.route('/cluster_num', methods=['GET', 'POST'])
